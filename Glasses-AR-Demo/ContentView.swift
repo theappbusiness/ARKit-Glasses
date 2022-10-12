53d7c4b29e783f2e43df126792542133bbd6a68b
Pete Murray <pete.murray@kinandcarta.com>
2022-10-12 10:45:33 +0100

    Initial Commit

---
+++ Glasses-AR-Demo/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Glasses-AR-Demo
+//
+//  Created by Pete Murray on 12/10/2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
