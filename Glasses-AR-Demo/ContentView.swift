738c44fda954e56ea0fe9c3b91e8ee1b385bf46d
Pete Murray <pete.murray@kinandcarta.com>
2022-10-13 11:59:01 +0100

    Add some example glasses scenes

--- Glasses-AR-Demo/ContentView.swift
+++ Glasses-AR-Demo/ContentView.swift
@@ -7,27 +7,46 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+
+	@State private var isPresented: Bool = false
+
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
-    }
+			.alert("Face Tracking Unavailable", isPresented: $isPresented) {
+				Button {
+					isPresented = false
+				} label: {
+					Text("Okay")
+				}
+			} message: {
+				Text("Face tracking requires an iPhone X or later.")
+			}
+			.onAppear {
+				if !ARFaceTrackingConfiguration.isSupported {
+					isPresented = true
+				}
+			}
+	}
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
+	func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        // Load the "Face" scene from the "Glasses" Reality File
+		if let faceScene = try? Glasses.loadFace() {
+			arView.scene.anchors.append(faceScene)
+		}
+
+		let arConfig = ARFaceTrackingConfiguration()
+		arView.session.run(arConfig)
+
         return arView
-        
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
